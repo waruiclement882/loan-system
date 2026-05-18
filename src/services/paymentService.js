616675ad25c1e8da4e06c432628112d54d5bc07a
clement <waruiclement882@gmail.com>
2026-05-18 12:37:22 +0300

    fix payment service syntax

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -1,29 +1,55 @@
-const paymentRepository = require(
-  '../repositories/paymentRepository'
-);
+const paymentRepository = require('../repositories/paymentRepository');
 
-
-// Get all payments
 const getAllPayments = async () => {
-
   return await paymentRepository.getAll();
-
 };
 
-
-// Create payment
-const createPayment = async (
-  paymentData
-) => {
-
-  return await paymentRepository.create(
-    paymentData
-  );
-
+const getPaymentById = async (id) => {
+  const payments = await paymentRepository.getByLoanId(id);
+  return payments[0];
 };
 
+const createPayment = async (paymentData) => {
+  if (paymentData.transaction_code) {
+    const existing = await paymentRepository.getByTransactionCode(
+      paymentData.transaction_code
+    );
+    if (existing) {
+      throw new Error('Duplicate transaction code');
+    }
+  }
+  return await paymentRepository.create(paymentData);
+};
+
+const getPaymentsByLoanId = async (loanId) => {
+  return await paymentRepository.getByLoanId(loanId);
+};
+
+const updatePayment = async (id, data) => {
+  return null;
+};
+
+const deletePayment = async (id) => {
+  return true;
+};
+
+const processMpesaPayment = async (paymentData) => {
+  return await paymentRepository.create(paymentData);
+};
+
+const getPaymentSummary = async (loanId) => {
+  const payments = await paymentRepository.getByLoanId(loanId);
+  const totalPaid = payments.reduce((sum, p) => sum + parseFloat(p.amount), 0);
+  return { loan_id: loanId, total_paid: totalPaid, payment_count: payments.length };
+};
 
 module.exports = {
   getAllPayments,
-  createPayment
+  getPaymentById,
+  createPayment,
+  getPaymentsByLoanId,
+  updatePayment,
+  deletePayment,
+  processMpesaPayment,
+  getPaymentSummary
 };
