96ce7164edcaea1b6eccb0476e5047d230d02495
clement <waruiclement882@gmail.com>
2026-05-29 15:28:12 +0300

    Fix Next.js 16 Turbopack config - remove next-pwa webpack conflict

--- loan-frontend/next.config.ts
+++ loan-frontend/next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
