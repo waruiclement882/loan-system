0b876493ec7045033f169ee5c96b970b9e9bda65
clement <waruiclement882@gmail.com>
2026-07-04 16:03:34 +0300

    Fix: export assignBranch, update branches route

--- src/routes/branches.js
+++ src/routes/branches.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db/pool');
+const pool = require('../db/connection');
 const { verifyToken, requireRole } = require('../middlewares/authMiddleware');
 
 // Get all branches
